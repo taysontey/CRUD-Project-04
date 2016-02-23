bfd4e254b5fafedd453790b1b52ad09c37448607
taysontey <taysontey@gmail.com>
2016-02-23 09:37:22 -0300

    Insert/Update/Delete Time(1)

---
+++ Projeto.DAL/Persistence/TimeDal.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Projeto.Entity.Entities;
+
+namespace Projeto.DAL.Persistence
+{
+    public class TimeDal : Conexao
+    {
+        public void Insert(Time t)
+        {
+            try
+            {
+                OpenConnection();
+
+                Cmd = new SqlCommand("INSERT INTO TB_TIME(NOME, DATAFUNDACAO) VALUES(@v1, @v2)", Con);
+                Cmd.Parameters.AddWithValue("@v1", t.Nome);
+                Cmd.Parameters.AddWithValue("@v2", t.DataFundacao);
+                Cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        public void Update(Time t)
+        {
+            try
+            {
+                OpenConnection();
+
+                Cmd = new SqlCommand("UPDATE TB_TIME SET NOME = @v1, DATAFUNDACAO = @v2 WHERE IDTIME = @v3", Con);
+                Cmd.Parameters.AddWithValue("@v1", t.Nome);
+                Cmd.Parameters.AddWithValue("@v2", t.DataFundacao);
+                Cmd.Parameters.AddWithValue("@v3", t.IdTime);
+                Cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        public void Delete(int Id)
+        {
+            try
+            {
+                OpenConnection();
+
+                Cmd = new SqlCommand("DELETE FROM TB_TIME WHERE IDTIME = @v1", Con);
+                Cmd.Parameters.AddWithValue("@v1", Id);
+                Cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+    }
+}
