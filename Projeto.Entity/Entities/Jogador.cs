e64691ea92475f38f10c51abf2065f892be09a0c
taysontey <taysontey@gmail.com>
2016-02-23 09:01:50 -0300

    Entity Mapping(1)

---
+++ Projeto.Entity/Entities/Jogador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entity.Entities
+{
+    public class Jogador
+    {
+        public int IdJogador { get; set; }
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Posicao { get; set; }
+        public int IdTime { get; set; }
+
+        #region Relacionamentos
+
+        public Time Time { get; set; }
+
+        #endregion
+    }
+}
