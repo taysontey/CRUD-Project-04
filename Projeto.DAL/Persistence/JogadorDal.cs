d6dea731b765881a8ce92e252bb610a6f581acb9
taysontey <taysontey@gmail.com>
2016-02-24 10:17:25 -0300

    Insert, Update, Delete Jogador(1)

---
+++ Projeto.DAL/Persistence/JogadorDal.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Projeto.Entity.Entities;
+
+namespace Projeto.DAL.Persistence
+{
+    public class JogadorDal : Conexao
+    {
+        public void Insert(Jogador j)
+        {
+            try
+            {
+                OpenConnection();
+
+                Cmd = new SqlCommand("INSERT INTO TB_JOGADOR(NOME, APELIDO, DATANASCIMENTO, POSICAO, IDTIME) VALUES(@v1, @v2, @v3, @v4, @v5)", Con);
+                Cmd.Parameters.AddWithValue("@v1", j.Nome);
+                Cmd.Parameters.AddWithValue("@v2", j.Apelido);
+                Cmd.Parameters.AddWithValue("@v3", j.DataNascimento);
+                Cmd.Parameters.AddWithValue("@v4", j.Posicao);
+                Cmd.Parameters.AddWithValue("@v5", j.IdTime);
+                Cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        public void Update(Jogador j)
+        {
+            try
+            {
+                OpenConnection();
+
+                Cmd = new SqlCommand("UPDATE TB_JOGADOR SET NOME = @v1, APELIDO = @v2, DATANASCIMENTO = @v3, POSICAO = @v4, IDTIME = @v5 WHERE IDJOGADOR = @v6", Con);
+                Cmd.Parameters.AddWithValue("@v1", j.Nome);
+                Cmd.Parameters.AddWithValue("@v2", j.Apelido);
+                Cmd.Parameters.AddWithValue("@v3", j.DataNascimento);
+                Cmd.Parameters.AddWithValue("@v4", j.Posicao);
+                Cmd.Parameters.AddWithValue("@v5", j.IdTime);
+                Cmd.Parameters.AddWithValue("@v6", j.IdJogador);
+                Cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        public void Delete(int Id)
+        {
+            try
+            {
+                OpenConnection();
+
+                Cmd = new SqlCommand("DELETE FROM TB_JOGADOR WHERE IDJOGADOR = @v1", Con);
+                Cmd.Parameters.AddWithValue("@v1", Id);
+                Cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        public List<Jogador> FindAll()
+        {
+            try
+            {
+                OpenConnection();
+
+                Cmd = new SqlCommand("SELECT * FROM TB_JOGADOR", Con);
+                Dr = Cmd.ExecuteReader();
+
+                List<Jogador> lista = new List<Jogador>();
+
+                while(Dr.Read())
+                {
+                    Jogador j = new Jogador();
+                    j.IdJogador = Convert.ToInt32(Dr["IDJOGADOR"]);
+                    j.Nome = Convert.ToString(Dr["NOME"]);
+                    j.Apelido = Convert.ToString(Dr["APELIDO"]);
+                    j.DataNascimento = Convert.ToDateTime(Dr["DATANASCIMENTO"]);
+                    j.Posicao = Convert.ToString(Dr["POSICAO"]);
+                    j.IdTime = Convert.ToInt32(Dr["IDTIME"]);
+
+                    lista.Add(j);
+                }
+
+                return lista;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+    }
+}
--- Projeto.DAL/Persistence/TimeDal.cs
+++ Projeto.DAL/Persistence/TimeDal.cs
@@ -20,7 +20,6 @@ namespace Projeto.DAL.Persistence
                 Cmd.Parameters.AddWithValue("@v1", t.Nome);
                 Cmd.Parameters.AddWithValue("@v2", t.DataFundacao);
                 Cmd.ExecuteNonQuery();
-
             }
             catch (Exception e)
             {
@@ -43,7 +42,6 @@ namespace Projeto.DAL.Persistence
                 Cmd.Parameters.AddWithValue("@v2", t.DataFundacao);
                 Cmd.Parameters.AddWithValue("@v3", t.IdTime);
                 Cmd.ExecuteNonQuery();
-
             }
             catch (Exception e)
             {
@@ -64,6 +62,38 @@ namespace Projeto.DAL.Persistence
                 Cmd = new SqlCommand("DELETE FROM TB_TIME WHERE IDTIME = @v1", Con);
                 Cmd.Parameters.AddWithValue("@v1", Id);
                 Cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        public List<Time> FindAll()
+        {
+            try
+            {
+                OpenConnection();
+                Cmd = new SqlCommand("SELECT * FROM TB_TIME", Con);
+                Dr = Cmd.ExecuteReader();
+
+                List<Time> lista = new List<Time>();
+
+                while(Dr.Read())
+                {
+                    Time t = new Time();
+                    t.IdTime = Convert.ToInt32(Dr["IDTIME"]);
+                    t.Nome = Convert.ToString(Dr["NOME"]);
+                    t.DataFundacao = Convert.ToDateTime(Dr["DATAFUNDACAO"]);
+
+                    lista.Add(t);
+                }
+
+                return lista;
             }
             catch (Exception e)
             {
